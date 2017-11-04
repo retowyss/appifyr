b194c791f3dccc3d8d6e98421c571773fc855faa
retowyss <reto.wyss.ch@gmail.com>
2017-11-04 03:08:32 +0100

    adjust tests

--- tests/testthat/test_args_to_json.R
+++ tests/testthat/test_args_to_json.R
@@ -5,32 +5,21 @@ context("Arguments To JSON")
 test_that("arg_to_json is character", {
   expect_type(arg_to_json("A", "42"), "character")
 })
-test_that("arg_to_json result", {
-  r <- "B : (function(){\nvar x = $('#B-xGkrZCuuI').val();\nreturn(isNaN() ? x : parseFloat(x));\n})()"
-  expect_equal(arg_to_json("B", "xGkrZCuuI"), r)
-  r <- "A : (function(){\nvar x = $('#A-123').val();\nreturn(isNaN() ? x : parseFloat(x));\n})()"
-  expect_equal(arg_to_json("A", "123"), r)
-})
 
 test_that("args_to_json is character", {
-  expect_type(arg_to_json(list("bar"), "1337"), "character")
+  expect_type(args_to_json(list("bar"), "1337"), "character")
 })
-test_that("args_to_json result", {
-  r <- "A : (function(){\nvar x = $('#A-123').val();\nreturn(isNaN() ? x : parseFloat(x));\n})()"
-  expect_equal(args_to_json("A", "123"), r)
-  r <- "A : (function(){\nvar x = $('#A-123').val();\nreturn(isNaN() ? x : parseFloat(x));\n})(),\nB : (function(){\nvar x = $('#B-123').val();\nreturn(isNaN() ? x : parseFloat(x));\n})(),\nC : (function(){\nvar x = $('#C-123').val();\nreturn(isNaN() ? x : parseFloat(x));\n})()"
-  expect_equal(args_to_json(list("A", "B", "C"), "123"), r)
+
+test_that("args_to_json is equal to arg_to_json", {
+  expect_equal(args_to_json(list("bar"), "1337"), args_to_json("bar", "1337"))
 })
 
 context("rplot")
 
 test_that("rplot is character", {
   expect_type(rplot(id = 5, rf = "rnorm", json = "n : 4"), "character")
-})
-
-test_that("rplot result", {
-  id = 5
-  json <- args_to_json(list("A", "B", "C"), id = id)
-  expect_equal(rplot(id = id, rf = "rnorm", json = json), "$(\"#submit-5\").click(function(){\nvar req = $(\"#target-5\").rplot(\n  \"rnorm\",\n  {A : (function(){\nvar x = $('#A-5').val();\nreturn(isNaN() ? x : parseFloat(x));\n})(),\nB : (function(){\nvar x = $('#B-5').val();\nreturn(isNaN() ? x : parseFloat(x));\n})(),\nC : (function(){\nvar x = $('#C-5').val();\nreturn(isNaN() ? x : parseFloat(x));\n})()}\n);\nreq.fail(function(){\n  alert(Error:  + req.responseText);\n});\n});")
+  id <- "1337"
+  json <- args_to_json(list("bar", "foo", "derp"), id)
+  expect_type(rplot(id = id, rf = "rnorm", json = json), "character")
 })
 
