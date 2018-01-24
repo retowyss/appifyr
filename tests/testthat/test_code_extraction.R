22df9d4e37ab1d82595974231589c471b86db3d3
retowyss <reto.wyss.ch@gmail.com>
2018-01-24 23:54:33 +0100

    add testcases for parsing quoted package names in require and library calls

--- tests/testthat/test_code_extraction.R
+++ tests/testthat/test_code_extraction.R
@@ -6,6 +6,10 @@ test_that("extract_pkg_name extracts the package name", {
   expect_equal(extract_pkg_names("require(tidyverse)"), "tidyverse")
   expect_equal(extract_pkg_names("library(appifyr)"), "appifyr")
   expect_equal(extract_pkg_names("library(keras)"), "keras")
+  expect_equal(extract_pkg_names("require('appifyr')"), "appifyr")
+  expect_equal(extract_pkg_names("require('tidyverse')"), "tidyverse")
+  expect_equal(extract_pkg_names("library('appifyr')"), "appifyr")
+  expect_equal(extract_pkg_names("library('keras')"), "keras")
 })
 
 
