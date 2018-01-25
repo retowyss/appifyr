7ded5031ba121e4639a494102b4a96e8b43da445
retowyss <reto.wyss.ch@gmail.com>
2018-01-25 03:50:48 +0100

    create more tests, reformat test code, exclude file-system manipulation code from testing

--- tests/testthat/test_args_to_json.R
+++ tests/testthat/test_args_to_json.R
@@ -11,24 +11,17 @@ test_that("args_to_json is character", {
 })
 
 test_that("args_to_json is equal to arg_to_json for single arg", {
-  expect_equal(
-    appifyr:::args_to_json(list("bar"), "1337"),
-    appifyr:::args_to_json("bar", "1337")
-  )
+  expect_equal(appifyr:::args_to_json(list("bar"), "1337"),
+               appifyr:::args_to_json("bar", "1337"))
 })
 
 context("rplot")
 
 test_that("rplot is character", {
-  expect_type(
-    appifyr:::rplot(id = 5, rf = "rnorm", json = "n : 4"),
-    "character"
-  )
   id <- "1337"
   json <- appifyr:::args_to_json(list("bar", "foo", "derp"), id)
-  expect_type(
-    appifyr:::rplot(id = id, rf = "rnorm", json = json),
-    "character"
-  )
-})
+  expect_type(appifyr:::rplot(id = id, rf = "rnorm", json = json), "character")
+  expect_type(appifyr:::rplot(id = 5, rf = "rnorm", json = "n : 4"),
+              "character")
 
+})
--- tests/testthat/test_code_extraction.R
+++ tests/testthat/test_code_extraction.R
@@ -1,15 +1,26 @@
 library(appifyr)
-context("Extractors: Package Names")
+library(testthat)
+context("Extractors")
 
 test_that("extract_pkg_name extracts the package name", {
-  expect_equal(extract_pkg_names("require(appifyr)"), "appifyr")
-  expect_equal(extract_pkg_names("require(tidyverse)"), "tidyverse")
-  expect_equal(extract_pkg_names("library(appifyr)"), "appifyr")
-  expect_equal(extract_pkg_names("library(keras)"), "keras")
-  expect_equal(extract_pkg_names("require('appifyr')"), "appifyr")
-  expect_equal(extract_pkg_names("require('tidyverse')"), "tidyverse")
-  expect_equal(extract_pkg_names("library('appifyr')"), "appifyr")
-  expect_equal(extract_pkg_names("library('keras')"), "keras")
+  # No quotes
+  expect_equal(appifyr:::extract_pkg_names("require(foo)"), "foo")
+  expect_equal(appifyr:::extract_pkg_names("library(foo)"), "foo")
+  # Single quotes
+  expect_equal(appifyr:::extract_pkg_names("require('bar')"), "bar")
+  expect_equal(appifyr:::extract_pkg_names("library('bar')"), "bar")
+  # Double quotes
+  expect_equal(appifyr:::extract_pkg_names('require("baz")'), "baz")
+  expect_equal(appifyr:::extract_pkg_names('library("baz")'), "baz")
 })
 
 
+test_that("extract_r_functions extracts r functions", {
+  r_fun <- "\nfoo <- function(x) {\n  x\n}"
+  expect_equal(appifyr:::extract_r_functions(r_fun), list(r_fun))
+})
+
+test_that("extract_r_functions extracts r functions", {
+  expect_equal(appifyr:::extract_pkgs("library(foo)")[[1]], "library(foo)")
+  expect_equal(appifyr:::extract_pkgs("require(foo)")[[2]], "require(foo)")
+})
